9d01f1372734f4d9434fb5735c3802548baa249e
PsyKickGuy <shubhdas2005@gmail.com>
2025-11-28 01:25:01 +0530

    progress_6

--- server/routes/factCheck.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const factCheckController = require("../controllers/factCheck.js");
-const router = express.Router();
-
-// POST /api/check
-router.post("/check", factCheckController.checkClaim);
-
-module.exports = router;
